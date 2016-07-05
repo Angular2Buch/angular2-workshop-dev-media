b7b90f444a406b6ec095bc7966a869868e8c5d84
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2016-07-05 15:32:15 +0200

    import { DashboardComponent } from './dashboard/';

--- book-rating/src/app/app.component.ts
+++ book-rating/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { DashboardComponent } from './dashboard/';
 
 @Component({
   moduleId: module.id,
   selector: 'br-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  styleUrls: ['app.component.css'],
+  directives: [DashboardComponent]
 })
 export class AppComponent {
   title = 'Angular 2 works!';
