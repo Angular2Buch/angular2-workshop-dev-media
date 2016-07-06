84c1904d05729b951a735ed0991c0f267be96eb1
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2016-07-06 11:32:51 +0200

    testing the book!

--- book-rating/src/app/shared/book.spec.ts
+++ book-rating/src/app/shared/book.spec.ts
@@ -9,7 +9,46 @@ import {
 import {Book} from './book';
 
 describe('Book', () => {
-  it('should create an instance', () => {
-    expect(new Book('adas', 'adasd')).toBeTruthy();
+  
+  xit('should create an instance', () => {
+    expect(new Book('Test', 'Description')).toBeTruthy();
   });
+
+  it('should have a default rating of 0', () => {
+    expect(new Book('Test', 'Description').rating).toEqual(0);
+  });
+
+
+  describe('Rating a book', () => {
+    let book: Book;
+
+    beforeEach(() => { book = new Book('Title', 'Description'); });
+
+    it('should increase the rating by one when it is rated up', () => {
+      book.rateUp();
+      expect(book.rating).toEqual(1);
+    });
+
+    it('should decrease the rating by one when it is rated down', () => {
+      book.rateUp();
+      book.rateDown();
+      expect(book.rating).toEqual(0);
+    });
+  });
+
+  describe('Rating a book (new business rules)', () => {
+
+    it('should not be allowed to have a rating greater than 5', () => {
+      let book = new Book('Title', 'Description', 5);
+      book.rateUp();
+      expect(book.rating).toEqual(5);
+    });
+
+    it('should not be possible have a rating smaller than 0', () => {
+      let book = new Book('Title', 'Description', 0);
+      book.rateDown();
+      expect(book.rating).toEqual(0);
+    });
+  });
+
 });
--- book-rating/src/app/shared/book.ts
+++ book-rating/src/app/shared/book.ts
@@ -1,9 +1,23 @@
 export class Book {
+
+    private MAX_RATING = 5;
+    private MIN_RATING = 0;
+
     constructor(public title: string,
-                public description: string,
-                public rating = 0) { }
+        public description: string,
+        public rating = 0) { }
 
-    // rateUp
+    rateUp() {
+        if (this.rating + 1 > this.MAX_RATING) {
+            return;
+        }
+        this.rating++;
+    }
 
-    // rateDown
+    rateDown() {
+        if (this.rating - 1 < this.MIN_RATING) {
+            return;
+        }
+        this.rating--;
+    }
 }
